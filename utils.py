93e9e153b42a004a0559367936adc0649cf9e55f
Kudo <rotlbatman1411@gmail.com>
2024-03-18 07:32:16 +0700

    update

--- utils.py
+++ utils.py
@@ -36,7 +36,7 @@ classes = [
     "figure skating pairs",
     "figure skating women",
     "fly fishing",
-    "football (UK)",
+    "football (US)",
     "formula 1 racing",
     "frisbee",
     "gaga",
@@ -118,20 +118,33 @@ classes = [
 def predict_label(img, model, classes):
     try:
         # Resize the image to (224, 224)
-        resized_img = tf.image.resize(img, (224, 224)).numpy().astype(int)
-        exp_img = np.expand_dims(resized_img, 0)
+        resized_img = tf.image.resize(img, (224, 224))
+        exp_img = np.expand_dims(resized_img, axis=0)
 
         # Predict with the model
         y_prob = model.predict(exp_img)
+        max_prob = np.max(y_prob)  # Get the maximum probability
 
-        if y_prob.max(axis=-1) < 0.5:
-            return "Cannot predict. Please input an appropriate image."  # No label, just a message
+        if max_prob < 0.5:
+            return "Cannot predict. Please input an appropriate image.", None  # No label, just a message
         else:
             y_classes = y_prob.argmax(axis=-1)
             label = classes[y_classes[0]]
-            return label.capitalize()  # Only the label
+
+            # Convert max_prob to a percentage and round to 2 decimal places
+            accuracy_percentage = round(max_prob * 100, 2)
+
+            # After rounding, convert to string and append the '%' symbol
+            accuracy_percentage_str = f"{accuracy_percentage}%"
+            return label.capitalize(), accuracy_percentage_str  # Return both label and accuracy percentage string
+
     except Exception as e:
-        return f"An error occurred: {e}"
+        return f"An error occurred: {e}", None
+
+
+
+
+
 
 
 
